36fb5a097804544af680439c8599b17a685eb177
Văn Lâm Nguyễn <76512317+lamhieo02@users.noreply.github.com>
2022-11-16 00:44:27 +0700

    change file CRUD API

--- AddEditDeleteFind_CRUD_Classes_Teachers_Courses.sql
+++ AddEditDeleteFind_CRUD_Classes_Teachers_Courses.sql
@@ -1,4 +1,4 @@
-
+﻿
 -- Add Course
 
 create procedure AddCourse @name nvarchar(40), @target float, @no_Lessons int, @price int
@@ -6,22 +6,15 @@ as
 	insert into Courses values(@name, @target, @no_Lessons, @price)
 go
 
--- Edit Course
+-- Update Course
 
-create procedure EditCourse @id int, @name nvarchar(40), @target float, @no_lessons int, @price int
+create procedure UpdateCourse @id int, @name nvarchar(40), @target float, @no_lessons int, @price int
 as
 	update Courses set Name = @name, Target = @target, No_Lessons = @no_lessons, Price = @price where ID = @id
 go
 
--- Delete Course
-
-create procedure DeleteCourse @id int
-as
-	delete from Courses where ID = @id
-go
-
--- Find Course By Id
-create procedure FindCourseByID @id int
+-- Search Course By Id
+create procedure SearchCourseByID @id int
 as
 	select * from Courses where ID = @id
 go
@@ -33,53 +26,41 @@ as
 	insert into Teachers values(@username, @date_Birth, @address, @name, @email, @phone)
 go
 
--- Edit Teacher
-create procedure EditTeacher @username varchar(100), @date_Birth date, @address nvarchar(100),
+-- Update Teacher
+create procedure UpdateTeacher @username varchar(100), @date_Birth date, @address nvarchar(100),
 @name nvarchar(50), @email varchar(50), @phone varchar(11)
 as
 	update Teachers set Date_Birth = @date_Birth, Address = @address, Name = @name, Email = @email,
 	Phone = @phone where Username = @username
 go
 
--- Delete Teacher
-create procedure DeleteTeacher @username varchar(100)
-as
-	delete from Teachers where Username = @username
-go
-
--- Find Teacher By Username
-create procedure FindTeacherByUsername @username varchar(100)
+-- Search Teacher By Username
+create procedure SearchTeacherByUsername @username varchar(100)
 as 
 	select * from Teachers where Username = @username
 go
 
 -- Add Class
 create procedure AddClass @name nvarchar(100), @start_Date date, @end_Date date, @teacher_ID varchar(100),
-@course_ID int, @weekDays varchar(10), @start_Time time, @end_Time time
+@course_ID int, @weekDays varchar(10), @start_Time time, @end_Time time, @classRoom varchar(20)
 as
 	insert into Classes values(@name, @start_Date, @end_Date, @teacher_ID, @course_ID, @weekDays,
-	@start_Time, @end_Time)
+	@start_Time, @end_Time, @classRoom, 0)
 go
 
--- Edit Class
-create procedure EditClass @id int, @name nvarchar(100), @start_Date date, @end_Date date, @teacher_ID varchar(100),
+-- Update Class By Id
+-- Không cập nhật số lượng học viên đang học trong lớp, vì số lượng học viên sẽ được cập nhật dựa theo table Class_Student
+create procedure UpdateClass @id int, @name nvarchar(100), @start_Date date, @end_Date date, @teacher_ID varchar(100),
 @course_ID int, @weekDays varchar(10), @start_Time time, @end_Time time
 as
 	update Classes set Name = @name, Start_Date = @start_Date, 
-	End_Date = @end_Date, Teacher_ID = @teacher_ID, Course_ID = @course_ID,WeekDays = @weekDays,
+	End_Date = @end_Date, Username = @teacher_ID, Course_ID = @course_ID,WeekDays = @weekDays,
 	Start_Time = @start_Time, End_Time = @end_Time where ID = @id
 go
 
---Delete Class
+--Search Class By ID
 
-create procedure DeleteClass @id int
-as
-	delete from Classes where ID = @id
-go
-
---Find Class By ID
-
-create procedure FindClassByID @id int
+create procedure SearchClassByID @id int
 as
 	select * from Classes where ID = @id
 go
--- CacChucNangKhac.sql
+++ CacChucNangKhac.sql
@@ -1,29 +1,45 @@
 
-use LanguageCenter
+use LanguageCenter3
 go
 
--- FindClass By Name
-create procedure dbo.FindClassByName(@classname nvarchar(100))
-as 
-	(select * from Classes where lower(Name) = lower(@classname));
-go
-
--- FindList Class by CourseName | note: select (cl.*) -> only select fields from table Class
-create function dbo.FindClassesByCName(@courseName nvarchar(40))
+-- Search List Class by CourseName | note: select (cl.*) -> only select fields from table Class
+create function dbo.SearchClassesByCName(@courseName nvarchar(40))
 returns table
 as
-	return (select cl.* from Classes cl inner join Courses c on cl.Course_ID = c.ID where lower(c.Name) = lower(@courseName) );
+	return (select cl.* 
+			from Classes cl inner join Courses c on cl.Course_ID = c.ID 
+			where lower(c.Name) = lower(@courseName) );
 go
 
--- FindCourse By Range Price (a-b)
-create function dbo.FindCourseByPrice(@lowerBound int,  @upperBound int)
+-- Search Course By Range Price (a-b)
+create function dbo.SearchCourseByPrice(@lowerBound int,  @upperBound int)
 returns table
 as
-	return (select * from Courses where Price >= @lowerBound and Price <= @upperBound);
+	return (select * 
+			from Courses 
+			where Price >= @lowerBound and Price <= @upperBound);
 go
 
--- FindTeacher By Name
-create procedure dbo.FindTeacherName(@teacherName nvarchar(50))
+-- Search Teacher By Name
+create procedure dbo.SearchTeacherName(@teacherName nvarchar(50))
 as
-	select * from Teachers where lower(Name) = lower(@teacherName)
+	select * 
+	from Teachers
+	where lower(Name) = lower(@teacherName)
 go	
+
+-- Search Class By Name
+create procedure dbo.SearchClassByName(@classname nvarchar(100))
+as 
+	select * 
+	from Classes
+	where lower(Name) = lower(@classname);
+go
+
+-- Search Info Teacher Is Teaching That Class
+create procedure dbo.SearchInfoTeacherByClass (@classname nvarchar(100))
+as
+	select t.* 
+	from Classes c inner join Teachers t on c.Username = t.Username
+	where c.Name = @classname
+go
